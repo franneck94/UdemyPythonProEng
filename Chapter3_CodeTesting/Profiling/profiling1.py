636df584638256df0f785b7ea04a62691df8fc68
franneck94 <schaffranek.jan@web.de>
2022-08-24 13:59:52 +0200

    up

--- Chapter3_CodeTesting/Profiling/profiling1.py
+++ Chapter3_CodeTesting/Profiling/profiling1.py
@@ -1,43 +1,48 @@
-'''Test code.
-'''
-# pylint: disable=import-error
+"""Test code.
+"""
 import cProfile
 import io
 import pstats
 import random
-import sys
 from functools import wraps
+from pathlib import Path
+from typing import Any, Callable
 
-from Chapter3_CodeTesting.Profiling.vector import Vector2D
+from vector import Vector2D
 
 
-def profile(fn):
+FILE_PATH = Path(__file__).parent.joinpath("profiling_stats.prof")
+
+
+def profile(fn: Callable) -> Callable:
     @wraps(fn)
-    def profiler(*args, **kwargs):
+    def profiler(*args: Any, **kwargs: Any) -> Any:
         profiler = cProfile.Profile()
         profiler.enable()
         fn_result = fn(*args, **kwargs)
         profiler.disable()
         stream = io.StringIO()
-        ps = pstats.Stats(profiler, stream=stream).sort_stats('time')
-        ps.print_stats()
+        stats = pstats.Stats(profiler, stream=stream)
+        stats.sort_stats(pstats.SortKey.TIME)
+        stats.print_stats()
         print(stream.getvalue())
+        stats.dump_stats(filename=FILE_PATH)
         return fn_result
+
     return profiler
 
 
 @profile
-def test_addition_own_implementation():
+def test_addition_own_implementation() -> None:
     for _ in range(100_000):
         v1 = Vector2D(random.randint(-10, 10), random.randint(-10, 10))
         v2 = Vector2D(random.randint(-10, 10), random.randint(-10, 10))
-        v3 = v1 + v2  # noqa
+        c3 = v1 + v2  # noqa
 
 
-def main() -> int:
+def main() -> None:
     test_addition_own_implementation()
-    return sys.exit(0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
