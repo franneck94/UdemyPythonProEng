a91bd304e5ba151910a30f7a3aaffda0e13a1de0
franneck94 <schaffranek.jan@web.de>
2022-08-26 14:02:26 +0200

    added

---
+++ Chapter4_Packages/Packaging_3/my_package/__init__.py
@@ -0,0 +1,9 @@
+from .utils.printing import print_hello_world
+from .utils.printing import print_name
+from .version import __version__
+
+__all__ = [
+    "print_hello_world",
+    "print_name",
+    "__version__"
+]
