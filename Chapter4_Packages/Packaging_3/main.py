c5d39a8febd3614972564b21cf6c1459f0b4c679
franneck94 <schaffranek.jan@web.de>
2022-08-27 12:49:21 +0200

    up

--- Chapter4_Packages/Packaging_3/main.py
+++ Chapter4_Packages/Packaging_3/main.py
@@ -1,10 +1,13 @@
 from my_package.utils.printing import print_hello_world
 from my_package.utils.printing import print_name
 
+from my_package import *
+
 
 def main() -> None:
     print_hello_world()
     print_name("Jan")
+    print(__version__)
 
 
 if __name__ == "__main__":
--- Chapter4_Packages/Packaging_3/my_package/__init__.py
+++ Chapter4_Packages/Packaging_3/my_package/__init__.py
@@ -1,9 +1,6 @@
-from .utils.printing import print_hello_world
-from .utils.printing import print_name
 from .version import __version__
 
+
 __all__ = [
-    "print_hello_world",
-    "print_name",
     "__version__"
 ]
