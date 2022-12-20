2578293b33d36450065089065e6b960a79e7a950
frankcaoyun <frankprvt@gmail.com>
2022-12-20 20:20:14 +0800

    update with pylint

--- Chapter02/PEP8/test_pylint.py
+++ Chapter02/PEP8/test_pylint.py
@@ -1,26 +1,33 @@
 # pylint: disable=invalid-name
+"""
+import standard libraries
+"""
 from math import sqrt
 from functools import total_ordering
 
 
 @total_ordering
 class Vector2D:
+    """
+    Testing docstring
+    """
+
     def __init__(self, x=0, y=0):
         if isinstance(x, float) and isinstance(y, float):
             self.x = x
             self.y = y
         else:
-            raise TypeError('You must pass in int/float values for x and y!')
+            raise TypeError("You must pass in int/float values for x and y!")
 
     def __call__(self):
         print("Calling the __call__ function!")
         return self.__repr__()
 
     def __repr__(self):
-        return f'vector.Vector2D({self.x}, {self.y})'
+        return f"vector.Vector2D({self.x}, {self.y})"
 
     def __str__(self):
-        return f'({self.x}, {self.y})'
+        return f"({self.x}, {self.y})"
 
     def __bool__(self):
         return bool(abs(self))
@@ -29,9 +36,10 @@ class Vector2D:
         return sqrt(pow(self.x, 2) + pow(self.y, 2))
 
     def check_vector_types(self, vector2):
+        """
+        Testing docstring"""
         if not isinstance(self, Vector2D) or not isinstance(vector2, Vector2D):
-            raise TypeError(
-                'You have to pass in two instances of the vector class!')
+            raise TypeError("You have to pass in two instances of the vector class!")
 
     def __eq__(self, other_vector):
         self.check_vector_types(other_vector)
@@ -65,12 +73,11 @@ class Vector2D:
             return self.x * other.x + self.y * other.y
         if isinstance(other, float):
             return Vector2D(self.x * other, self.y * other)
-        raise TypeError(
-            'You must pass in a vector instance or an int/float number!')
+        raise TypeError("You must pass in a vector instance or an int/float number!")
 
     def __truediv__(self, other):
         if isinstance(other, float):
             if other != 0.0:
                 return Vector2D(self.x / other, self.y / other)
-            raise ValueError('You cannot divide by zero!')
-        raise TypeError('You must pass in an int/float value!')
+            raise ValueError("You cannot divide by zero!")
+        raise TypeError("You must pass in an int/float value!")
