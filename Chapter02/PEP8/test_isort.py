2d02bbb0154f035ef3a50684986f555bf36cfca5
franneck94 <schaffranek.jan@web.de>
2023-05-18 09:00:16 +0200

    up

--- Chapter02/PEP8/test_isort.py
+++ Chapter02/PEP8/test_isort.py
@@ -6,7 +6,12 @@ import sys
 import numpy
 import pandas
 
-from my_lib import Object, Object2, Object3, Object4, Object5, Object6
+from my_lib import Object
+from my_lib import Object2
+from my_lib import Object3
+from my_lib import Object4
+from my_lib import Object5
+from my_lib import Object6
 
 
 print("Hey")
