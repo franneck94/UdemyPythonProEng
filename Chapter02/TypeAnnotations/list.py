340614c323feaeacc98fe59c34781b4f4c597f6b
franneck94 <schaffranek.jan@web.de>
2023-11-18 13:07:54 +0100

    up

--- Chapter02/TypeAnnotations/list.py
+++ Chapter02/TypeAnnotations/list.py
@@ -1,13 +1,16 @@
 def append_list(
-    lst: list[float] | list[int],
-    val: float | int,
+    lst: list[int],
+    val: int,
 ) -> None:
-    lst.append(val)  # type: ignore
+    lst.append(val)
 
 
 def main() -> None:
     lst = [1, 2, 3]
     append_list(lst, 4)
+
+    # lst2 = [1.0, 2.0, 3.0]
+    # append_list(lst2, 4)
 
 
 if __name__ == "__main__":
--- Chapter02/TypeAnnotations/tuple.py
+++ Chapter02/TypeAnnotations/tuple.py
@@ -1,31 +1,31 @@
+from typing import Union
+
+
 def print_3d_tuple(
-    tpl: tuple[int | float, bool, str],
+    tpl: tuple[int, float, str],
 ) -> None:
     for val in tpl:
         print(val)
 
 
 def print_nd_int_tuple(
-    tpl: tuple[int, ...],
+    tpl: tuple[Union[int, float], ...],
 ) -> None:
     for val in tpl:
         print(val)
 
 
 def main() -> None:
-    tpl = (1, True, "")
-    print_3d_tuple(tpl)
+    tpl1 = (1, 1.0, "1")
+    print_3d_tuple(tpl1)
+    # tpl2 = (1.0, 1.0, "1")
+    # print_3d_tuple(tpl2)
 
-    tpl2 = (1.0, True, "")
-    print_3d_tuple(tpl2)
-
-    # tpl3 = (1, True)
-    # print_3d_tuple(tpl3)
-
-    tpl4 = (1, 2, 3)
+    tpl3 = (1,)
+    print_nd_int_tuple(tpl3)
+    tpl4 = (1, 2)
     print_nd_int_tuple(tpl4)
-
-    tpl5 = (1, 2)
+    tpl5 = (1.0,)
     print_nd_int_tuple(tpl5)
 
 
